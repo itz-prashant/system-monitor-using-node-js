25aa2a1b95314a480b6153a30e920ab8be512d97
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-02-22 16:13:53 +0530

    Add fs:promise to write file

--- fs.js
+++ fs.js
@@ -1,37 +1,51 @@
 // import * as fs from "node:fs";
 import * as fs from "node:fs/promises";
 
-function createFile(pathname) {
-  // Aync api
-  // console.log("1")
-  // fs.writeFileSync(pathname, "Hello node js\n")
-  // console.log("2")
-  // fs.appendFileSync(pathname, "Hello javascript")
-  // console.log("File has been created")
+// function createFile(pathname) {
+//   // Aync api
+//   // console.log("1")
+//   // fs.writeFileSync(pathname, "Hello node js\n")
+//   // console.log("2")
+//   // fs.appendFileSync(pathname, "Hello javascript")
+//   // console.log("File has been created")
 
-  // Async Api
-  // Error first call back
-  
-  console.log("File operation start!");
+//   // Async Api
+//   // Error first call back
 
-  fs.writeFile(pathname, "Hello node js\n", function (err) {
-    if (err) {
-      console.log("Something went wrong while creating file");
-      return;
+//   console.log("File operation start!");
+
+//   fs.writeFile(pathname, "Hello node js\n", function (err) {
+//     if (err) {
+//       console.log("Something went wrong while creating file");
+//       return;
+//     }
+
+//     fs.appendFile(pathname, "Hello Javascript", function (err) {
+//       if (err) {
+//         console.log("Something went wrong while append file");
+//         return;
+//       }
+//       console.log("File has been append asynchoronusly");
+//     });
+
+//     console.log("File has been created asynchoronusly");
+//   });
+
+//   console.log("File operation done!");
+// }
+
+// createFile("./hello.txt");
+
+
+async function createFile(pathname) {
+    
+    try {
+        await fs.writeFile(pathname, 'Hello node js\n')
+        await fs.appendFile(pathname, 'Hello Javascript\n')
+    } catch (error) {
+        console.log(error)
     }
-
-    fs.appendFile(pathname, "Hello Javascript", function (err) {
-      if (err) {
-        console.log("Something went wrong while append file");
-        return;
-      }
-      console.log("File has been append asynchoronusly");
-    });
-
-    console.log("File has been created asynchoronusly");
-  });
-
-  console.log("File operation done!");
+    console.log('File written !')
 }
 
-createFile("./hello.txt");
+createFile('./hello-promise.txt')
